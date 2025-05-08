391890673e5908fb8ec72993ceec9d1b77fbedc0
Daniel Na <DNA@DESKTOP-GKC3M84>
2025-05-08 18:22:57 -0400

    Updated Image/Text/CSS & Added Raindrops/Music

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+//import { useState } from 'react'
+import './App.css'
+import ReactAudioPlayer from 'react-audio-player'
+import React from 'react';
+ 
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={"/Fp14ZO5aYAAv1LH.jpg"} className="my-soul" alt="my-life" />
+      </div>
+      <h1>It Never Gets Any Better</h1>
+      <div className="card">
+      </div>
+    </>
+  )
+}
+export default App
+
+ 
---
+++ src/Raindrop.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactRain from 'react-rain-animation';
+import "react-rain-animation/lib/style.css";
+
+const Raindrops = () => {
+ 
+    return (
+        <ReactRain
+          numDrops="50"
+        />
+    )
+  }
+
+  export default Raindrops;
